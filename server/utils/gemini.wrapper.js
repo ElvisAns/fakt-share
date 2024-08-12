7f8b986543039445e7fb81ac45688e7ab32359bf
ANSIMA Elvis <ansimapersic@gmail.com>
2024-08-12 12:09:10 +0200

    add generationconfig , increase topK for more random response

--- server/utils/gemini.wrapper.js
+++ server/utils/gemini.wrapper.js
@@ -4,10 +4,17 @@ const MODEL_NAME = "gemini-1.5-flash";
 const API_KEY = process.env.GOOGLE_GEMINI_API_KEY;
 
 async function execute(parts) {
+    const generationConfig = {
+        temperature: 0.9,
+        topK: 80, //higher the value, random will the response be
+        topP: 1,
+        maxOutputTokens: 2048,
+    };
     const genAI = new GoogleGenerativeAI(API_KEY);
     const model = genAI.getGenerativeModel({ model: MODEL_NAME });
     const result = await model.generateContent({
         contents: [{ role: "user", parts }],
+        generationConfig
     });
     const response = result.response.text();
     
